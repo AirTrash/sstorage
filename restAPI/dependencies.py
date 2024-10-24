a25cd3526ef4a47e5ec820407ef98bc33bae0eb6
Nikita <dzhasbayev@bk.ru>
2024-10-25 00:56:33 +0500

    deps

--- restAPI/dependencies.py
+++ restAPI/dependencies.py
@@ -1,3 +1,5 @@
+from fastapi import Depends
+from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
 from secret_manager.db.db_helper import db_helper
 
@@ -5,3 +7,6 @@ from secret_manager.db.db_helper import db_helper
 async def db_session() -> AsyncSession:
     async with db_helper.get_session() as session:
         yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(db_session)]
